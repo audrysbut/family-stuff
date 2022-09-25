9c153b8616c533e1e6098cabe845e9dd1e8a325f
Audrius <audrius.butkus@barbora.lt>
2022-09-25 19:22:15 +0300

    chore: added day 2 training for Jolita

--- src/pages/jolita/jolita-page.tsx
+++ src/pages/jolita/jolita-page.tsx
@@ -1,6 +1,7 @@
 import { ExerciseView } from "../../components/exercise-view/exercise-view";
 import { data as day1 } from "./training-data/day-1";
+import { data as day2 } from "./training-data/day-2";
 
-const trainingData = [day1];
+const trainingData = [day1, day2];
 
 export const JolitaPage = () => <ExerciseView trainingData={trainingData} />;
---
+++ src/pages/jolita/training-data/day-2.ts
@@ -0,0 +1,44 @@
+import { SportData } from "../../../components/exercise-view/sport-data";
+
+export const data: SportData[] = [
+  {
+    muscleGroup: "Apšilimas",
+    exercise: "Ėjimas+pramankštos pratimai",
+    series: ["10 min"],
+  },
+  {
+    muscleGroup: "Presas",
+    exercise: "Susirietimai + kojų kėlimai + kojos pėdų siekimai",
+    series: ["3x12", "3x12", "3x12"],
+  },
+  {
+    muscleGroup: "Nugara",
+    exercise: "10) Viršutinio troso trauka už galvos",
+    series: ["4x12 9.5 Kg"],
+  },
+  {
+    muscleGroup: "Nugara",
+    exercise: "1) Apatinio troso trauka į pilvą (siaura rankena)",
+    series: ["3x12 6 Kg"],
+  },
+  {
+    muscleGroup: "Nugara",
+    exercise: "Trauka su hanteliais gulint kampu",
+    series: ["3x15 3 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Spirimai su svareliais klupint+dubens kėlimai maišu",
+    series: ["3x15+3x15 10 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "14) Lenkimas sėdint",
+    series: ["3x15 15 Kg"],
+  },
+  {
+    muscleGroup: "Kojos",
+    exercise: "Suvedimai sėdint+skietimai sėdint (pasvirus į priekį)",
+    series: ["3x20+3x20"],
+  },
+];
