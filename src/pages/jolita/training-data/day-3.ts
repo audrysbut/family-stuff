cd91b681dbcecc1a996f6b25a811e2413316fdfa
Audrius <audrysbut@gmail.com>
2022-11-01 09:31:23 +0200

    feat: added 3 training day for Jolita

---
+++ src/pages/jolita/training-data/day-3.ts
@@ -0,0 +1,33 @@
+import { SportData } from "../../../components/exercise-view/sport-data";
+
+export const data: SportData[] = [
+  {
+    muscleGroup: "Apšilimas",
+    exercise: "Dviratis",
+    series: ["10 min"]
+  }, {
+    muscleGroup: "Presas",
+    exercise: "Susirietimai-kamuolio uždėjimas ant koju",
+    series: ["4x10"]
+  }, {
+    muscleGroup: "Krūtinė",
+    exercise: "Spausti hantelius gulint+spausti blyna gulint",
+    series: ["3x12 4 Kg", "3x12 10 Kg"]
+  }, {
+    muscleGroup: "Pečiai",
+    exercise: "Mostai su hanteliais per šalis+mostai su hanteliais ir guma į priekį",
+    series: ["3x12 2kg", "3x12 2kg"]
+  },{
+    muscleGroup: "Kojos",
+    exercise: "Kamuolio spaudimas sėdint+pritupimai 8 tada 10s pauzė pritūpus ir vėl 8 (baisiai lėti)",
+    series: ["3x40s", "3x16"]
+  },{
+    muscleGroup: "Kojos",
+    exercise: "Kelti koja su svareliais gulint šonu+kelti koja su svareliais gulit ant nugaros",
+    series: ["3x15","3x15"]
+  },{
+    muscleGroup: "Blauzdos",
+    exercise: "Blauzdų staklėse",
+    series: ["4x15"]
+  }
+];
